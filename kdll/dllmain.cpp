3564784d156990443f94a8a46c073795b57a89d3
andrey <originalnull@gmail.com>
2014-01-09 15:48:41 +0400

    fixed projects

--- kdll/dllmain.cpp
+++ kdll/dllmain.cpp
@@ -1,5 +1,5 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
-#include "stdafx.h"
+#include "project.h"
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
@@ -9,18 +9,19 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:
-		OutputDebugStringA("DLL_PROCESS_ATTACH");
+		OutputDebugStringA("DLL_PROCESS_ATTACH\n");
 		break;
 	case DLL_THREAD_ATTACH:
-		OutputDebugStringA("DLL_THREAD_ATTACH");
+		OutputDebugStringA("DLL_THREAD_ATTACH\n");
 		break;
 	case DLL_THREAD_DETACH:
-		OutputDebugStringA("DLL_THREAD_DETACH");
+		OutputDebugStringA("DLL_THREAD_DETACH\n");
 		break;
 	case DLL_PROCESS_DETACH:
-		OutputDebugStringA("DLL_PROCESS_DETACH");
+		OutputDebugStringA("DLL_PROCESS_DETACH\n");
 		break;
 	}
+
 	return TRUE;
 }
 
--- kdll/kdll.cpp
+++
@@ -1,6 +0,0 @@
-// kdll.cpp : Defines the exported functions for the DLL application.
-//
-
-#include "stdafx.h"
-
-
